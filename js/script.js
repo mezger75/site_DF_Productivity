6daf7fc4406d0f4b03164b296b2d79d2caf11747
mezger75 <9771804@gmail.com>
2022-10-12 23:54:27 +0500

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -8,6 +8,28 @@ btnNavEl.addEventListener("click", function () {
   navEl.classList.toggle("nav-open");
 });
 
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("list__link"))
+      navEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
